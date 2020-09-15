8cc45e50ff4fff7df1c1786c71522e6d2ac34c0f
Oscar Shoudt <shoudto@gmail.com>
2020-09-15 09:18:05 -0400

    Done.

--- app/models/song.rb
+++ app/models/song.rb
@@ -3,6 +3,7 @@ class Song < ActiveRecord::Base
   belongs_to :artist 
   belongs_to :genre
   has_many :notes
+  accepts_nested_attributes_for :notes 
 
   def artist_name=(name)
     self.artist = Artist.find_or_create_by(name: name)
@@ -12,11 +13,11 @@ class Song < ActiveRecord::Base
     self.artist ? self.artist.name : nil  
   end 
 
-  def genre_name=(genre)
-    self.genre = Genre.find_or_create_by(name: genre)
+  def genre_name=(name)
+    self.genre = Genre.find_or_create_by(name: name)
   end
 
   def genre_name
-    self.genre ? self.genre.name : nil 
+    self.genre ? self.genre.name : nil
   end
 end
